e7fd3c00cd75580b3a4cbc4310d30ef0e75e3ff0
Max Musing <max.musing@gmail.com>
2023-01-26 17:04:55 -0500

    Fix lint issues in new-dock

--- src/pages/new-dock.tsx
+++ src/pages/new-dock.tsx
@@ -2,9 +2,21 @@ import { useRouter } from "next/router";
 
 const NewDock = () => {
   const router = useRouter();
-  const queryParams = router.query
-  const appNames = Array.isArray(queryParams.app) ? queryParams.app : [queryParams.app ?? '']
-  return <div><h1 className={"text-xl mb-4"}>New dock page</h1><div className={'flex flex-col gap-y-2'}>{appNames.map(app => <div>{app}</div>)}</div></div>
-}
+  const queryParams = router.query;
+  const appNames = Array.isArray(queryParams.app)
+    ? queryParams.app
+    : [queryParams.app ?? ""];
+
+  return (
+    <div>
+      <h1 className={"mb-4 text-xl"}>New dock page</h1>
+      <div className={"flex flex-col gap-y-2"}>
+        {appNames.map((appName) => (
+          <div key={appName}>{appName}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default NewDock;
