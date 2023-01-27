33e061ce857e02f67634504ec3b7f2cfbe76691c
Robert Cooper <robert@robertcooper.me>
2023-01-27 18:18:53 -0500

    Await ssr

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -50,7 +50,7 @@ export const api = createTRPCNext<AppRouter>({
    * Whether tRPC should await queries when server rendering pages
    * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
    */
-  ssr: false,
+  ssr: true,
 });
 
 /**
