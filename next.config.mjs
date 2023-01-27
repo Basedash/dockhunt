f0af7074535ad562acd53fe18f81afd4fed65680
Robert Cooper <robert@robertcooper.me>
2023-01-27 16:29:47 -0500

    Ignore eslint on build

--- next.config.mjs
+++ next.config.mjs
@@ -30,5 +30,10 @@ const config = {
       },
     ],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 export default config;
