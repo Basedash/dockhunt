88022c4d98637560bbbb9bf854be74cc56572e12
Robert Cooper <robert@robertcooper.me>
2023-01-27 16:30:00 -0500

    Remove comment

--- next.config.mjs
+++ next.config.mjs
@@ -31,8 +31,6 @@ const config = {
     ],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
