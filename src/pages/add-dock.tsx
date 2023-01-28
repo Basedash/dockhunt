0422a1d2c1beceddd9f2b7bd7341709e6874f558
Robert Cooper <robert@robertcooper.me>
2023-01-28 17:39:07 -0500

    Fix invalid dom nesting

--- src/pages/add-dock.tsx
+++ src/pages/add-dock.tsx
@@ -10,26 +10,30 @@ const AddDock = () => {
         <meta name="description" content="Add your dock" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="flex min-h-screen flex-col items-start justify-center px-4 max-w-[800px]">
+      <div className="flex min-h-screen max-w-[800px] flex-col items-start justify-center px-4">
         <p className={"mb-8 text-xl"}>
           To add your own dock, run the following command in your terminal:
         </p>
         <code className="mb-8 w-full rounded border bg-black p-4">
           npx dockhunt
         </code>
+        <p className={"mb-8 text-xl"}>The command will:</p>
+        <ol className="mt-2 mb-8 list-decimal pl-8 text-xl">
+          <li>Find the apps in your dock</li>
+          <li>Upload any icons not yet in our database</li>
+          <li>Create a dock on this website</li>
+        </ol>
         <p className={"mb-8 text-xl"}>
-          The command will:
-          <ol className="mt-2 list-decimal pl-8">
-            <li>Find the apps in your dock</li>
-            <li>Upload any icons not yet in our database</li>
-            <li>Create a dock on this website</li>
-          </ol>
+          Dockhunt will only use the apps that are pinned to your dock. Those
+          are the apps you marked as "Stay in dock" when you right-clicked on
+          them and selected "Options". The apps that are not pinned will be
+          ignored.
         </p>
-        <p className={"mb-8 text-xl"}>
-          Dockhunt will only use the apps that are pinned to your dock. Those are the apps you marked as "Stay in dock" when
-          you right-clicked on them and selected "Options". The apps that are not pinned will be ignored.
-        </p>
-        <Image className={'mb-8'} src={pinnedDocks} alt={"Pinned vs recent dock items"} />
+        <Image
+          className={"mb-8"}
+          src={pinnedDocks}
+          alt={"Pinned vs recent dock items"}
+        />
         <a
           href="https://github.com/Basedash/dockhunt"
           target="_blank"
