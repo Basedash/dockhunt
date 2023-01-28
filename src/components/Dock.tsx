ee6827c97d7ed1eb1e5d814f112353258d4e837b
Robert Cooper <robert@robertcooper.me>
2023-01-28 17:22:05 -0500

    Add dock item labels

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import type { App } from "@prisma/client";
 import Link from "next/link";
+import * as Tooltip from "@radix-ui/react-tooltip";
 
 const DockItem = ({ app }: { app: App }) => {
   const variants = {
@@ -15,50 +16,65 @@ const DockItem = ({ app }: { app: App }) => {
   };
 
   return (
-    <Link href={`/apps/${app.name}`}>
-      <motion.div
-        variants={variants}
-        whileHover="hover"
-        initial="initial"
-        className="dockItem h-[60px]"
-        transition={{
-          type: "spring",
-          damping: 60,
-          stiffness: 500,
-          mass: 1,
-        }}
-      >
-        {/* TODO: Switch to next/image once we're storing icons in our own bucket. Might be tricky with framer motion though. */}
-        <motion.img
-          variants={{
-            hover: {
-              width: 92,
-              height: 92,
-              y: -12,
-            },
-            initial: {
-              width: 80,
-              height: 80,
-            },
-          }}
-          transition={{
-            type: "spring",
-            damping: 60,
-            stiffness: 500,
-            mass: 1,
-          }}
-          alt={`${app.name} app icon`}
-          title={app.name}
-          whileHover="hover"
-          initial="initial"
-          className={"absolute"}
-          src={
-            app.iconUrl ??
-            "https://dockhunt-images.nyc3.cdn.digitaloceanspaces.com/placeholder.png"
-          }
-        />
-      </motion.div>
-    </Link>
+    <Tooltip.Root>
+      <Tooltip.Trigger>
+        <Link href={`/apps/${app.name}`}>
+          <motion.div
+            variants={variants}
+            whileHover="hover"
+            initial="initial"
+            className="dockItem h-[60px]"
+            transition={{
+              type: "spring",
+              damping: 60,
+              stiffness: 500,
+              mass: 1,
+            }}
+          >
+            {/* TODO: Switch to next/image once we're storing icons in our own bucket. Might be tricky with framer motion though. */}
+            <motion.img
+              variants={{
+                hover: {
+                  width: 92,
+                  height: 92,
+                  y: -12,
+                },
+                initial: {
+                  width: 80,
+                  height: 80,
+                },
+              }}
+              transition={{
+                type: "spring",
+                damping: 60,
+                stiffness: 500,
+                mass: 1,
+              }}
+              alt={`${app.name} app icon`}
+              title={app.name}
+              whileHover="hover"
+              initial="initial"
+              className={"absolute"}
+              src={
+                app.iconUrl ??
+                "https://dockhunt-images.nyc3.cdn.digitaloceanspaces.com/placeholder.png"
+              }
+            />
+          </motion.div>
+        </Link>
+      </Tooltip.Trigger>
+      <Tooltip.Portal>
+        <Tooltip.Content sideOffset={10}>
+          <div
+            className={
+              "rounded-[4px] border border-[#49494B] bg-[#272728] py-[4px] px-[10px] text-xs text-white"
+            }
+          >
+            {app.name}
+          </div>
+        </Tooltip.Content>
+      </Tooltip.Portal>
+    </Tooltip.Root>
   );
 };
 
@@ -68,11 +84,13 @@ export function Dock({ apps }: { apps: App[] }) {
       {/* Dock background */}
       <div className="absolute bottom-0 left-0 right-0 h-[80px] max-w-full rounded-[22px] border border-gray-600/60 bg-gray-800/60 backdrop-blur" />
       {/* Scrollable container */}
-      <div className="relative flex max-w-full flex-1 overflow-x-auto pt-4">
-        {apps.map((app) => (
-          <DockItem key={app.name} app={app} />
-        ))}
-      </div>
+      <Tooltip.Provider delayDuration={0}>
+        <div className="relative flex max-w-full flex-1 overflow-x-auto pt-4">
+          {apps.map((app) => (
+            <DockItem key={app.name} app={app} />
+          ))}
+        </div>
+      </Tooltip.Provider>
     </div>
   );
 }
