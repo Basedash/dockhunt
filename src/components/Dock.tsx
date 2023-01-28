acc10dedc43070fc4da447bfc1897ab166ea29b3
Max Musing <max.musing@gmail.com>
2023-01-28 17:03:30 -0500

    Re-fix apps clipping on top on hover

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -66,14 +66,12 @@ export function Dock({ apps }: { apps: App[] }) {
   return (
     <div className="relative">
       {/* Dock background */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 h-[80px] max-w-full rounded-[22px] border border-gray-600/60 bg-gray-800/60 backdrop-blur" />
+      <div className="absolute bottom-0 left-0 right-0 h-[80px] max-w-full rounded-[22px] border border-gray-600/60 bg-gray-800/60 backdrop-blur" />
       {/* Scrollable container */}
-      <div className="relative max-w-full flex-1 overflow-x-auto">
-        <div className="flex min-w-min">
-          {apps.map((app) => (
-            <DockItem key={app.name} app={app} />
-          ))}
-        </div>
+      <div className="relative flex max-w-full flex-1 overflow-x-auto pt-4">
+        {apps.map((app) => (
+          <DockItem key={app.name} app={app} />
+        ))}
       </div>
     </div>
   );
