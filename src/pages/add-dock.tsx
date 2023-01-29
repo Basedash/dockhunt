3c90e11eb4211495016b311f690ea2386d136d2d
Max Musing <max.musing@gmail.com>
2023-01-29 16:19:25 -0500

    Add info on prerequisites to add dock page

--- src/pages/add-dock.tsx
+++ src/pages/add-dock.tsx
@@ -10,46 +10,65 @@ const AddDock = () => {
         <meta name="description" content="Add your dock" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="flex min-h-screen max-w-[800px] flex-col items-start justify-center px-8 py-20">
-        <p className={"mb-8 text-xl"}>
+      <div className="flex min-h-screen max-w-[800px] flex-col items-start justify-center px-8 py-24">
+        <h3 className="mb-2 text-xl">Prerequisites</h3>
+        <ul className="mb-8 list-disc pl-8 text-xl">
+          <li>
+            You must have{" "}
+            <a
+              href="https://nodejs.org"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-400 hover:underline"
+            >
+              Node.js
+            </a>{" "}
+            installed on your computer
+          </li>
+          <li>You will be asked to authenticate with Twitter</li>
+        </ul>
+        <p className="mb-8 text-xl">
           To add your own dock, run the following command in your terminal:
         </p>
         <code className="mb-8 w-full rounded border bg-black p-4">
           npx dockhunt
         </code>
-        <p className={"mb-8 text-xl"}>The command will:</p>
-        <ol className="mt-2 mb-8 list-decimal pl-8 text-xl">
+        <p className="mb-2 text-xl">The command will:</p>
+        <ol className="mb-8 list-decimal pl-8 text-xl">
           <li>Find the apps in your dock</li>
           <li>Upload any icons not yet in our database</li>
           <li>Create a dock on this website</li>
         </ol>
-        <p className={"mb-8 text-xl"}>
-          Dockhunt will only use the apps that are pinned to your dock. Those
-          are the apps you marked as "Stay in dock" when you right-clicked on
-          them and selected "Options". The apps that are not pinned will be
-          ignored.
+
+        <div className="mb-8 flex flex-col items-start text-xl">
+          <a
+            href="https://github.com/Basedash/dockhunt-cli"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            View source code on GitHub
+          </a>
+          <a
+            href="https://www.npmjs.com/package/dockhunt"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            View package on npm
+          </a>
+        </div>
+
+        <p className="mb-8 text-xl">
+          Dockhunt will only use the apps that are pinned to your dock. Apps can
+          be pinned by right-clicking and selecting Options &gt; Keep in Dock.
+          Apps that are not pinned will be ignored.
         </p>
         <Image
-          className={"mb-8"}
+          className="mb-8"
           src={pinnedDocks}
-          alt={"Pinned vs recent dock items"}
+          alt="Pinned vs recent dock items"
         />
-        <a
-          href="https://github.com/Basedash/dockhunt-cli"
-          target="_blank"
-          rel="noreferrer"
-          className="text-xl hover:underline"
-        >
-          View source code on GitHub
-        </a>
-        <a
-          href="https://www.npmjs.com/package/dockhunt"
-          target="_blank"
-          rel="noreferrer"
-          className="text-xl hover:underline"
-        >
-          View package on npm
-        </a>
       </div>
     </>
   );
