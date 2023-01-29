6a1f1e0a7b86ecc414d2d1ccc4bf4868b73b7269
Max Musing <max.musing@gmail.com>
2023-01-29 17:37:09 -0500

    Tweak AddDockCard copy

--- src/components/AddDockCard.tsx
+++ src/components/AddDockCard.tsx
@@ -13,8 +13,13 @@ export function AddDockCard() {
   return (
     <div className="relative">
       <div className="h-64 w-full rounded-[36px] border border-solid border-gray-600/60 bg-monterey bg-cover bg-center opacity-60" />
-      <div className="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center p-8 text-xl">
-        <p>Want to add your own dock? Just run:</p>
+      <div className="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center p-10 text-xl">
+        <p className="hidden text-center sm:block">
+          Want to add your own dock? Run this command in your terminal:
+        </p>
+        <p className="block text-center sm:hidden">
+          Want to add your own dock? Run this command on a macOS device:
+        </p>
         <code className="my-4 rounded border border-gray-100/60 bg-gray-900/70 p-4 backdrop-blur">
           npx dockhunt
         </code>
