3585b02ff37be4dc6a5b8c3e1dfe0d51e2097e51
Max Musing <max.musing@gmail.com>
2023-01-29 18:49:08 -0500

    Add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,12 @@ import { unstable_getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { DockCard } from "components/DockCard";
 import { AddDockCard } from "components/AddDockCard";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { data: sessionData } = useSession();
+  const user = api.users.getOne.useQuery({ id: sessionData?.user?.id ?? "" });
   const featuredDocks = api.docks.getFeatured.useQuery();
   const latestDocks = api.docks.getLatest.useQuery();
 
@@ -27,17 +31,34 @@ const Home: NextPage = () => {
           content="Discover the apps everyone is docking about"
         />
       </Head>
-      <div className="w-screen max-w-[80rem] px-6 py-24 md:px-20">
-        <h2 className="mb-12 text-3xl font-semibold">Featured docks</h2>
-        <div className="flex flex-col gap-20">
+      <div className="w-screen max-w-[80rem] px-6 py-24 md:py-32 md:px-20">
+        {!user.data?.dock && (
+          <div className="mb-16 flex flex-col items-center gap-4">
+            <h1 className="text-center text-4xl font-bold">
+              Discover the apps everyone is docking about
+            </h1>
+            <h2 className="text-center text-xl">
+              Share your dock and see who else has docked the apps you use
+            </h2>
+            <Link
+              href="/add-dock"
+              className="rounded-full bg-blue-700 px-4 py-2 hover:bg-blue-600"
+            >
+              Add your dock
+            </Link>
+          </div>
+        )}
+
+        <h3 className="mb-8 text-3xl font-semibold">Featured docks</h3>
+        <div className="flex flex-col gap-10 md:gap-16">
           {featuredDocks.data
             ? featuredDocks.data.map((dock) => (
                 <DockCard key={dock.id} dock={dock} />
               ))
             : "Loading..."}
         </div>
-        <h2 className="mt-24 mb-12 text-3xl font-semibold">Latest docks</h2>
-        <div className="flex flex-col gap-20">
+        <h3 className="mt-24 mb-8 text-3xl font-semibold">Latest docks</h3>
+        <div className="flex flex-col gap-10 md:gap-16">
           <AddDockCard />
 
           {latestDocks.data
