5f2f013a66309ea07981d7a5c389f0fcbe613f60
Robert Cooper <robert@robertcooper.me>
2023-01-28 19:45:02 -0500

    Test OG image

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -18,53 +18,80 @@ export default function handler(req: NextRequest, res: NextApiResponse) {
       (
         <div
           style={{
-            // backgroundImage: `url("${process.env.NEXTAUTH_URL as string}/og-wallpaper.jpg")`,
-            backgroundSize: 'cover',
-            width: '1200px',
-            height: '630px',
-            display: 'flex',
+            fontSize: 100,
+            color: 'black',
+            background: 'white',
+            width: '100%',
+            height: '100%',
+            padding: '50px 200px',
             textAlign: 'center',
-            alignItems: 'center',
             justifyContent: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
+            alignItems: 'center',
           }}
         >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              justifyItems: 'center',
-              marginBottom: '16px'
-            }}
-          >
-            {/*<img src={`${process.env.NEXTAUTH_URL as string}/og-dockhunt.png`} height={126} width={126} />*/}
-          </div>
-          <div
-            style={{
-              fontSize: 52,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              padding: '0 60px',
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
-              border: '1px solid rgba(75,85,99,.6)',
-              backgroundColor: 'rgba(31,41,55,.6)',
-              borderRadius: '15px',
-            }}
-          >
-            {`Checkout ${username}'s dock`}
-          </div>
+          👋, 🌎
         </div>
       ),
       {
         width: 1200,
         height: 630,
+        // Supported options: 'twemoji', 'blobmoji', 'noto', 'openmoji', 'fluent' and 'fluentFlat'
+        // Default to 'twemoji'
+        emoji: 'twemoji',
       },
     );
+
+    // return new ImageResponse(
+    //   (
+    //     <div
+    //       style={{
+    //         // backgroundImage: `url("${process.env.NEXTAUTH_URL as string}/og-wallpaper.jpg")`,
+    //         backgroundSize: 'cover',
+    //         width: '1200px',
+    //         height: '630px',
+    //         display: 'flex',
+    //         textAlign: 'center',
+    //         alignItems: 'center',
+    //         justifyContent: 'center',
+    //         flexDirection: 'column',
+    //         flexWrap: 'nowrap',
+    //       }}
+    //     >
+    //       <div
+    //         style={{
+    //           display: 'flex',
+    //           alignItems: 'center',
+    //           justifyContent: 'center',
+    //           justifyItems: 'center',
+    //           marginBottom: '16px'
+    //         }}
+    //       >
+    //         {/*<img src={`${process.env.NEXTAUTH_URL as string}/og-dockhunt.png`} height={126} width={126} />*/}
+    //       </div>
+    //       <div
+    //         style={{
+    //           fontSize: 52,
+    //           fontStyle: 'normal',
+    //           letterSpacing: '-0.025em',
+    //           color: 'white',
+    //           marginTop: 30,
+    //           padding: '0 60px',
+    //           lineHeight: 1.4,
+    //           whiteSpace: 'pre-wrap',
+    //           border: '1px solid rgba(75,85,99,.6)',
+    //           backgroundColor: 'rgba(31,41,55,.6)',
+    //           borderRadius: '15px',
+    //         }}
+    //       >
+    //         {`Checkout ${username}'s dock`}
+    //       </div>
+    //     </div>
+    //   ),
+    //   {
+    //     width: 1200,
+    //     height: 630,
+    //   },
+    // );
   } catch (e) {
     if (e instanceof Error) {
       console.log(`${e.message}`);
