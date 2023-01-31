a2b96848198532b1e818b7414ccba31e5f6b4d26
Robert Cooper <robert@robertcooper.me>
2023-01-31 11:21:54 -0500

    Horizontal scrollbar

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import { placeholderImageUrl } from "utils/constants";
 import Image from "next/image";
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 
 const DockImage = motion(Image);
 
@@ -85,11 +86,23 @@ export function Dock({ apps }: { apps: App[] }) {
       {/* Dock background */}
       <div className="absolute bottom-0 left-0 right-0 h-[80px] max-w-full rounded-[22px] border border-gray-600/60 bg-gray-800/60" />
       {/* Scrollable container */}
-      <div className="fade-lr relative flex max-w-full flex-1 overflow-x-auto pt-4">
-        {apps.map((app) => (
-          <DockItem key={app.name} app={app} />
-        ))}
-      </div>
+      <ScrollArea.Root>
+        <ScrollArea.Viewport>
+          <div className="fade-lr relative flex max-w-full flex-1 overflow-x-auto pt-4">
+            {apps.map((app) => (
+              <DockItem key={app.name} app={app} />
+            ))}
+          </div>
+        </ScrollArea.Viewport>
+        {/* TODO: Style the scrollbar: https://www.radix-ui.com/docs/primitives/components/scroll-area */}
+        <ScrollArea.Scrollbar orientation="vertical">
+          <ScrollArea.Thumb />
+        </ScrollArea.Scrollbar>
+        <ScrollArea.Scrollbar orientation="horizontal">
+          <ScrollArea.Thumb />
+        </ScrollArea.Scrollbar>
+        <ScrollArea.Corner />
+      </ScrollArea.Root>
     </div>
   );
 }
