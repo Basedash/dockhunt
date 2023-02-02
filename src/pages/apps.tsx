2d8eddca7abec0ea33aa5f58bc008b9feec45e90
Max Musing <max.musing@gmail.com>
2023-02-02 16:37:19 -0500

    Tweak behaviour

--- src/pages/apps.tsx
+++ src/pages/apps.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useState, useMemo } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,9 +12,14 @@ export default function Apps() {
   const [query, setQuery] = useState("");
 
   const filteredApps = useMemo(() => {
-    if (query.length < 2) return topApps.data;
-
-    return topApps.data?.filter((app) => app.name?.includes(query));
+    return topApps.data
+      ?.map((app, index) => ({
+        ...app,
+        position: index + 1,
+      }))
+      .filter((app) =>
+        app.name?.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+      );
   }, [topApps, query]);
 
   return (
@@ -25,26 +30,27 @@ export default function Apps() {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <div className="flex min-h-screen w-screen max-w-[80rem] flex-col items-start justify-center px-6 py-24 md:px-20">
+      <div className="flex min-h-screen w-screen max-w-[80rem] flex-col items-start px-6 py-24 md:px-20">
         <h1 className="mb-6 text-3xl font-semibold">Top 100 apps</h1>
         <div className="relative mb-8 flex w-full max-w-xs items-center">
           <input
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             placeholder="Search apps"
-            className="w-full rounded-md border border-gray-500 bg-gray-900 p-2 ring-blue-500/50 hover:bg-gray-800 focus:outline-none focus:ring-4"
+            className="w-full rounded-md border border-gray-600/60 bg-gray-900/60 p-2 ring-blue-500/50 hover:bg-gray-800/60 focus:outline-none focus:ring-4"
           />
-          <button
-            onClick={() => setQuery("")}
-            className={`absolute right-2 rounded-full bg-gray-800 p-1 px-2 text-sm text-gray-400 hover:bg-gray-700 hover:text-gray-200 ${
-              query.length < 1 ? "hidden" : ""
-            }`}
-          >
-            Clear
-          </button>
+          {query.length > 0 && (
+            <button
+              onClick={() => setQuery("")}
+              className="absolute right-2 rounded-full bg-gray-800 p-1 px-2 text-sm text-gray-400 hover:bg-gray-700 hover:text-gray-200"
+            >
+              Clear
+            </button>
+          )}
         </div>
+
         <div className="flex w-full flex-col divide-y divide-gray-600/60">
-          {filteredApps?.map((app, index) => (
+          {filteredApps?.map((app) => (
             <Link
               key={app.name}
               className="flex items-center gap-4 hover:bg-gray-600/60 sm:p-2"
@@ -57,7 +63,7 @@ export default function Apps() {
                 alt={`${app.name} app icon`}
               />
               <h2>
-                {index + 1}. {app.name}
+                {app.position}. {app.name}
               </h2>
               <div className="flex-grow" />
               <p>{app._count.dockItems} docks</p>
