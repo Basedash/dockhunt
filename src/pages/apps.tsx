223efe302b61d1563a60cfb5eabb7707c252bd5a
tedspare <ted.spare@gmail.com>
2023-02-02 12:26:28 -0500

    Add basic text filter to top app list

--- src/pages/apps.tsx
+++ src/pages/apps.tsx
@@ -1,3 +1,4 @@
+import React, { useMemo, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,6 +9,14 @@ export default function Apps() {
   const topApps = api.apps.getTop.useQuery();
   const dockCount = api.docks.getCount.useQuery();
 
+  const [query, setQuery] = useState("");
+
+  const filteredApps = useMemo(() => {
+    if (query.length < 2) return topApps.data;
+
+    return topApps.data?.filter((app) => app.name?.includes(query));
+  }, [topApps, query]);
+
   return (
     <>
       <Head>
@@ -17,9 +26,25 @@ export default function Apps() {
       </Head>
 
       <div className="flex min-h-screen w-screen max-w-[80rem] flex-col items-start justify-center px-6 py-24 md:px-20">
-        <h1 className="mb-8 text-3xl font-semibold">Top 100 apps</h1>
+        <h1 className="mb-6 text-3xl font-semibold">Top 100 apps</h1>
+        <div className="relative mb-8 flex w-full max-w-xs items-center">
+          <input
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search apps"
+            className="w-full rounded-md border border-gray-500 bg-gray-900 p-2 ring-blue-500/50 hover:bg-gray-800 focus:outline-none focus:ring-4"
+          />
+          <button
+            onClick={() => setQuery("")}
+            className={`absolute right-2 rounded-full bg-gray-800 p-1 px-2 text-sm text-gray-400 hover:bg-gray-700 hover:text-gray-200 ${
+              query.length < 1 ? "hidden" : ""
+            }`}
+          >
+            Clear
+          </button>
+        </div>
         <div className="flex w-full flex-col divide-y divide-gray-600/60">
-          {topApps.data?.map((app, index) => (
+          {filteredApps?.map((app, index) => (
             <Link
               key={app.name}
               className="flex items-center gap-4 hover:bg-gray-600/60 sm:p-2"
